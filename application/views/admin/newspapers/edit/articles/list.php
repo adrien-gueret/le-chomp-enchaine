89447331cf6338c2fea8bd090efa4b03a0d5d86c
Adrien Guéret <76342@supinfo.com>
2015-08-02 17:40:24 +0200

    fix(admin): don't try to access protected properties anymore

--- application/views/admin/newspapers/edit/articles/list.php
+++ application/views/admin/newspapers/edit/articles/list.php
@@ -11,22 +11,22 @@
 	<tbody>
 	<?php foreach($view->articles as $article): ?>
 		<tr>
-			<td><?= $article->id; ?></td>
+			<td><?= $article->getId(); ?></td>
 			<td>
-				<a href="<?= $view->base_url; ?>admin/articles/edit?id=<?= $article->id; ?>">
-					<?= $article->title; ?>
+				<a href="<?= $view->base_url; ?>admin/articles/edit?id=<?= $article->getId(); ?>">
+					<?= $article->prop('title'); ?>
 				</a>
 			</td>
 			<td>
-				<?= $article->author_username; ?>
+				<?= $article->load('author')->prop('username'); ?>
 			</td>
 			<td>
-				Le <?= date('d/m/Y à H:i', strtotime($article->date_last_update)); ?>
+				Le <?= date('d/m/Y à H:i', strtotime($article->prop('date_last_update'))); ?>
 			</td>
 			<td>
 				<form action="<?= $view->base_url; ?>admin/newspapers/edit/removeArticle?id=<?= $view->id_newspaper; ?>" method="post">
 					<input type="hidden" name="__method__" value="PUT" />
-					<input type="hidden" name="id_article" value="<?= $article->id; ?>" />
+					<input type="hidden" name="id_article" value="<?= $article->getId(); ?>" />
 					<input type="submit" value="Dé-lier" />
 				</form>
 			</td>
