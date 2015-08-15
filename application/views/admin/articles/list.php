8f050ccf2d605b4f20b9df1dc8a6da2c5dd55e8e
Tailszefox <tails@tailszefox.net>
2015-08-15 20:33:06 +0200

    feat(admin/articles): add link to read article

--- application/views/admin/articles/list.php
+++ application/views/admin/articles/list.php
@@ -4,19 +4,21 @@
 			<th>Rubrique</th>
 			<th>Titre</th>
 			<th>Dernière modification</th>
+			<th>Actions</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php foreach($view->articles as $article): ?>
 			<tr>
 				<td><?= $article->load('section')->prop('name'); ?></td>
-				<td>
-					<a href="<?= $view->base_url; ?>admin/articles/edit?id=<?= $article->getId(); ?>">
-						<?= $article->prop('title'); ?>
-					</a>
-				</td>
+				<td><?= $article->prop('title'); ?></td>
 				<td>
 					Le <?= date('d/m/Y à H:i', strtotime($article->prop('date_last_update'))); ?>
+				</td>
+				<td>
+					<a href="<?= $view->base_url; ?>admin/articles/edit?id=<?= $article->getId(); ?>">Éditer</a>
+					 | 
+					<a href="<?= $article->getUrl() ?>">Lire</a>
 				</td>
 			</tr>
 		<?php endforeach; ?>
