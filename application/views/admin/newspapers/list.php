872a08e5e9f33d775be9ff7352811f56692cd939
Tailszefox <tails@tailszefox.net>
2015-08-28 19:32:24 +0200

    feat(views/admin/newspapers): add link to view each newspaper

--- application/views/admin/newspapers/list.php
+++ application/views/admin/newspapers/list.php
@@ -4,23 +4,25 @@
 			<th>ID</th>
 			<th>Nom</th>
 			<th>Date de publication</th>
+			<th>Actions</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php foreach($view->all_newspapers as $newspapers): ?>
 			<tr>
 				<td><?= $newspapers->getId(); ?></td>
-				<td>
-					<a href="<?= $view->base_url; ?>admin/newspapers/edit?id=<?= $newspapers->getId(); ?>">
-						<?= $newspapers->prop('name'); ?>
-					</a>
-				</td>
+				<td><?= $newspapers->prop('name'); ?></td>
 				<td>
 					<?php if(!$newspapers->prop('date_publication')): ?>
 						Non publié
 					<?php else: ?>
 						Le <?= date('d/m/Y à H:i', strtotime($newspapers->prop('date_publication'))); ?>
 					<?php endif; ?>
+				</td>
+				<td>
+					<a href="<?= $view->base_url; ?>admin/newspapers/edit?id=<?= $newspapers->getId(); ?>">Éditer</a>
+					 | 
+					<a href="<?= $newspapers->getUrl() ?>">Lire</a>
 				</td>
 			</tr>
 		<?php endforeach; ?>
