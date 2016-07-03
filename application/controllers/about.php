e4ff2fa6dd176709156902d2df9d17b5a5342bfd
Adrien Guéret <76342@supinfo.com>
2016-07-03 17:37:51 +0200

    fix(categories): categories can be added from admin again

--- application/controllers/about.php
+++ application/controllers/about.php
@@ -1,7 +1,7 @@
 <?php
 	class Controller_about extends Controller_index
 	{
-		public function get_index()
+		public function get_index($unused_param = 0)
 		{
 			$this->response->set(\Eliya\Tpl::get('about/index'));
 		}
