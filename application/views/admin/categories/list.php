eba6413100a62296cdc17853598bd6b931b667c1
Adrien Guéret <76342@supinfo.com>
2016-07-13 22:07:21 +0200

    feat(admin): update categories layout

--- application/views/admin/categories/list.php
+++ application/views/admin/categories/list.php
@@ -1,13 +1,15 @@
-<table>
-	<tbody>
-		<?php foreach($view->all_categories as $category): ?>
-			<tr>
-				<td><img src="<?= $category->getMainPictureUrl(); ?>" /></td>
-				<th><h3><?= $category->prop('name'); ?></h3></th>
-				<td>
-					<a href="<?= $view->base_url; ?>admin/categories/edit?id=<?= $category->getId(); ?>">Éditer</a>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
+<div class="list-of-cards">
+	<?php foreach($view->all_categories as $category): ?>
+		<article class="card-list category">
+			<a href="<?= $view->base_url; ?>admin/categories/edit?id=<?= $category->getId(); ?>">
+				<figure>
+					<img src="<?= $category->getMainPictureURL(); ?>" alt="" />
+					<figcaption>
+						<h3><?= $category->prop('name'); ?></h3>
+						<p><?= $category->prop('description'); ?></p>
+					</figcaption>
+				</figure>
+			</a>
+		</article>
+	<?php endforeach; ?>
+</div>
