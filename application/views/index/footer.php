0f7bbb4f44d0e5291e51c2bd6378f22cfd33fcec
Adrien Guéret <adrien.grt@gmail.com>
2023-04-10 00:09:53 +0200

    feat(footer): remove FB link

--- application/views/index/footer.php
+++ application/views/index/footer.php
@@ -1,7 +1,6 @@
 		</main>
 		<footer>
 			<nav class="social-networks">
-				<a href="https://www.facebook.com/lechompenchaine/" title="Facebook"></a>
 				<a href="https://twitter.com/MarioUnivRsalis/" title="Twitter"></a>
 				<a href="https://github.com/adrien-gueret/le-chomp-enchaine/" title="GitHub"></a>
 			</nav>
